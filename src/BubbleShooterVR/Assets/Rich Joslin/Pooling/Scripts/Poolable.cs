264b32056ba605317d723e14ee5a5799f61c90d0
Rich Joslin <richjoslin@richjoslin.com>
2018-08-27 13:46:27 -0700

    moved stuff; added docfx stuff

---
+++ src/BubbleShooterVR/Assets/Rich Joslin/Pooling/Scripts/Poolable.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace RichJoslin
+{
+	namespace Pooling
+	{
+		[System.Serializable]
+		public class Poolable
+		{
+			public string key;
+			public GameObject prefab;
+			public int amountToSpawn;
+		}
+	}
+}
---
+++ src/BubbleShooterVR/Assets/Rich Joslin/Pooling/Scripts/PooledInstanceInterfaces.cs
@@ -0,0 +1,16 @@
+﻿
+namespace RichJoslin
+{
+	namespace Pooling
+	{
+		public interface IPooledInstanceGet
+		{
+			void OnGet(UnityEngine.Object arg = null);
+		}
+
+		public interface IPooledInstanceReturn
+		{
+			void OnReturn();
+		}
+	}
+}
